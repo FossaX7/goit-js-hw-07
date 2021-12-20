e6f60268e2478e8afc0d1eee792b6d982d7c241e
Andrii Mogylnytskyi <88145575+FossaX7@users.noreply.github.com>
2021-12-21 00:40:47 +0200

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsGallery = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsGallery);
+
+galleryContainer.addEventListener('click', onContainerGalleryClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join('');
+};
+
+function onContainerGalleryClick(evt) {
+    evt.preventDefault();
+     const modalPicture = basicLightbox.create(`
+     <img src="${evt.target.dataset.source}">
+    `)
+    modalPicture.show(() => escModalClose(modalPicture));
+    
+};
+
+function escModalClose(modalPicture) {
+    document.addEventListener("keydown", evt => {
+        if (evt.code === 'Escape'){
+            modalPicture.close()
+        }
+    })
+};
